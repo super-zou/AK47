ae8ff6bc69ed69f18d698e82ee2ea2bfc4558bb0
U-UCLOUDLINK\haichao.zou <haichao.zou@bangong-0008.ucloudlink.com>
2017-09-07 19:28:02 +0800

    improve login

--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/Login.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/Login.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,6 +23,10 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 import com.tongmenhui.launchak47.util.Slog;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import static java.security.AccessController.getContext;
 
 public class Login extends AppCompatActivity {
@@ -31,7 +36,12 @@ public class Login extends AppCompatActivity {
     private Button loginBtn;
     private ProgressDialog progressDialog;
     //Login form info
-    private final String  domain = "http://www.tongmenhui.com";
+    private String account;
+    private String password;
+    private static final String  domain = "http://www.tongmenhui.com";
+    private static final String token_url = domain + "?q=rest_services/user/token";
+    private static final String check_url = domain + "?q=account_manager/check_login_user";
+    private static final String login_url = domain + "?q=rest_services/user/login";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,16 +57,16 @@ public class Login extends AppCompatActivity {
             @Override
             public void onClick(View view){
                int type = 0;
-               String account = accountEdit.getText().toString();
-               String password = passwordEdit.getText().toString();
+                account = accountEdit.getText().toString();
+                password = passwordEdit.getText().toString();
                 Slog.d(TAG, "account: "+account+" password: "+password);
-                String accountCheckUrl = domain + "?q=account_manager/check_login_user";
                 RequestBody requestBody = new FormBody.Builder()
                         .add("type", "0")
                         .add("account", account)
                         .add("password", password)
                         .build();
-                toLogin(accountCheckUrl, requestBody);
+                showProgress();
+                goto_check(check_url, requestBody);
 
             }
         });
@@ -65,13 +75,110 @@ public class Login extends AppCompatActivity {
 
     }
 
-    private void toLogin(String address, RequestBody requestBody){
-        showProgress();
-        HttpUtil.sendOkHttpRequest(address, requestBody, new Callback(){
+    private void goto_check(String address, RequestBody requestBody){
+
+        HttpUtil.sendOkHttpRequest(null, address, requestBody, new Callback(){
+            int check_login_user = 0;
+            String user_name;
+
             @Override
             public void onResponse(Call call, Response response) throws IOException{
                 String responseText = response.body().string();
                 Slog.d(TAG, "response : "+responseText);
+                if(!TextUtils.isEmpty(responseText)){
+                    try {
+                        JSONObject check_response= new JSONObject(responseText);
+                        check_login_user = Integer.parseInt(check_response.getString("check_login_user"));
+                        user_name = check_response.getString("user_name");
+                        Slog.d(TAG, "check_login_user: "+check_response.getString("check_login_user"));
+                        Slog.d(TAG, "user_name: "+check_response.getString("user_name"));
+
+                        if(check_login_user != 0){
+                            goto_login(user_name);
+                        }
+
+
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e){
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run(){
+                        closeProgressDialog();
+                        Toast.makeText(Login.this, "登录失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    private void goto_login(String user_name){
+        access_token(user_name);
+    }
+
+    private void access_token(final String user_name){
+        HttpUtil.sendOkHttpRequest(null, token_url, null, new Callback(){
+            int check_login_user = 0;
+            //String username = user_name;
+            String token;
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException{
+                Slog.d(TAG, "response token: "+response.body().string());
+                token = response.body().string();
+                login_finally(token, user_name);
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e){
+                runOnUiThread(new Runnable(){
+                    @Override
+                    public void run(){
+                        closeProgressDialog();
+                        Toast.makeText(Login.this, "登录失败", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+    }
+
+    private void login_finally(String token, String user_name){
+        RequestBody requestBody = new FormBody.Builder()
+                .add("username", user_name)
+                .add("password", password)
+                .build();
+
+        HttpUtil.sendOkHttpRequest(token, login_url, requestBody, new Callback(){
+            int check_login_user = 0;
+            String user_name;
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException{
+                String responseText = response.body().string();
+                Slog.d(TAG, "response : "+responseText);
+                if(!TextUtils.isEmpty(responseText)){
+                    try {
+                        JSONObject check_response= new JSONObject(responseText);
+                        check_login_user = Integer.parseInt(check_response.getString("check_login_user"));
+                        user_name = check_response.getString("user_name");
+                        Slog.d(TAG, "check_login_user: "+check_response.getString("check_login_user"));
+                        Slog.d(TAG, "user_name: "+check_response.getString("user_name"));
+
+                        if(check_login_user != 0){
+                            goto_login(user_name);
+                        }
+
+
+                    }catch (JSONException e){
+                        e.printStackTrace();
+                    }
+                }
             }
 
             @Override
--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/util/HttpUtil.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/util/HttpUtil.java
@@ -9,9 +9,28 @@ import okhttp3.RequestBody;
  */
 
 public class HttpUtil {
-    public static void sendOkHttpRequest(String address, RequestBody requestBody, okhttp3.Callback callback){
+    public static void sendOkHttpRequest(String header, String address, RequestBody requestBody, okhttp3.Callback callback){
         OkHttpClient client = new OkHttpClient();
-        Request request = new Request.Builder().url(address).post(requestBody).build();
+        Request request = null;
+
+        if(requestBody != null){
+            if(header != null){
+                request = new Request.Builder().url(address).header("X-CSRF-Token", header).post(requestBody).build();
+            }else{
+                request = new Request.Builder().url(address).post(requestBody).build();
+            }
+
+        }else{
+            if(header != null){
+                request = new Request.Builder().url(address).header("X-CSRF-Token", header).build();
+            }else{
+                request = new Request.Builder().url(address).build();
+            }
+
+        }
+
         client.newCall(request).enqueue(callback);
     }
+
+
 }
