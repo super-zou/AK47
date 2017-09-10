cf2e2a30b83d6017407212fc3bc32db59a74e63a
super.zou <super.zou@emali.com>
2017-09-10 22:57:26 +0800

    improve

---
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tongmenhui.launchak47.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tongmenhui.launchak47.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
