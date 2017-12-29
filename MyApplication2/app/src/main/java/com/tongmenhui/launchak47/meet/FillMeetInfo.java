094c8fae73eb385234d112d8fb828b60422724d9
super.zou <super.zou@emali.com>
2017-12-29 12:52:44 +0800

    improve

--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/meet/FillMeetInfo.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/meet/FillMeetInfo.java
@@ -1,5 +1,6 @@
 package com.tongmenhui.launchak47.meet;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -14,6 +15,7 @@ import org.angmarch.views.NiceSpinner;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FillMeetInfo extends AppCompatActivity {
 
@@ -24,9 +26,48 @@ public class FillMeetInfo extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        NiceSpinner niceSpinner = (NiceSpinner) findViewById(R.id.nice_spinner);
-        List<String> dataset = new LinkedList<>(Arrays.asList("One", "Two", "Three", "Four", "Five"));
-        niceSpinner.attachDataSource(dataset);
+
+        Resources res =getResources();
+        String[] years = res.getStringArray(R.array.years);
+        String[] months = res.getStringArray(R.array.months);
+        String[] days = res.getStringArray(R.array.days);
+        String[] heights = res.getStringArray(R.array.heights);
+        String[] degrees = res.getStringArray(R.array.degrees);
+        String[] regions = res.getStringArray(R.array.regions);
+        String[] provinces = res.getStringArray(R.array.provinces);
+        String[] cities = res.getStringArray(R.array.cities);
+
+        NiceSpinner niceSpinnerYears = (NiceSpinner) findViewById(R.id.nice_spinner_years);
+        List<String> yearList = new LinkedList<>(Arrays.asList(years));
+        niceSpinnerYears.attachDataSource(yearList);
+
+        NiceSpinner niceSpinnerMonths = (NiceSpinner) findViewById(R.id.nice_spinner_months);
+        List<String> monthList = new LinkedList<>(Arrays.asList(months));
+        niceSpinnerMonths.attachDataSource(monthList);
+
+        NiceSpinner niceSpinnerDays = (NiceSpinner) findViewById(R.id.nice_spinner_days);
+        List<String> dayList = new LinkedList<>(Arrays.asList(days));
+        niceSpinnerDays.attachDataSource(dayList);
+
+        NiceSpinner niceSpinnerHeight = (NiceSpinner) findViewById(R.id.nice_spinner_height);
+        List<String> heightList = new LinkedList<>(Arrays.asList(heights));
+        niceSpinnerHeight.attachDataSource(heightList);
+
+        NiceSpinner niceSpinnerDegree = (NiceSpinner) findViewById(R.id.nice_spinner_degree);
+        List<String> degreeList = new LinkedList<>(Arrays.asList(degrees));
+        niceSpinnerDegree.attachDataSource(degreeList);
+
+        NiceSpinner niceSpinnerRegion = (NiceSpinner) findViewById(R.id.nice_spinner_region);
+        List<String> regionList = new LinkedList<>(Arrays.asList(regions));
+        niceSpinnerRegion.attachDataSource(regionList);
+
+        NiceSpinner niceSpinnerProvince = (NiceSpinner) findViewById(R.id.nice_spinner_province);
+        List<String> provinceList = new LinkedList<>(Arrays.asList(provinces));
+        niceSpinnerProvince.attachDataSource(provinceList);
+
+        NiceSpinner niceSpinnerCity = (NiceSpinner) findViewById(R.id.nice_spinner_city);
+        List<String> cityList = new LinkedList<>(Arrays.asList(cities));
+        niceSpinnerCity.attachDataSource(cityList);
     }
 
 }
