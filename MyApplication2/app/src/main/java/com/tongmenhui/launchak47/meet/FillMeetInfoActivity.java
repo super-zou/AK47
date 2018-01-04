4c8f5bf0bb1975cff4952be7ddaf378cc9e530a5
super.zou <super.zou@emali.com>
2018-01-04 23:05:10 +0800

    improve

--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/meet/FillMeetInfoActivity.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/meet/FillMeetInfoActivity.java
@@ -7,10 +7,14 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import com.tongmenhui.launchak47.R;
+import com.tongmenhui.launchak47.util.Slog;
 
 import org.angmarch.views.NiceSpinner;
 
@@ -54,36 +58,101 @@ public class FillMeetInfoActivity extends AppCompatActivity {
 
 
         NiceSpinner niceSpinnerYears = (NiceSpinner) findViewById(R.id.nice_spinner_years);
-        List<String> yearList = new LinkedList<>(Arrays.asList(years));
+        final List<String> yearList = new LinkedList<>(Arrays.asList(years));
         niceSpinnerYears.attachDataSource(yearList);
+        niceSpinnerYears.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(yearList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerMonths = (NiceSpinner) findViewById(R.id.nice_spinner_months);
         List<String> monthList = new LinkedList<>(Arrays.asList(months));
         niceSpinnerMonths.attachDataSource(monthList);
+        niceSpinnerYears.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(yearList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerDays = (NiceSpinner) findViewById(R.id.nice_spinner_days);
         List<String> dayList = new LinkedList<>(Arrays.asList(days));
         niceSpinnerDays.attachDataSource(dayList);
+        niceSpinnerYears.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(yearList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerHeight = (NiceSpinner) findViewById(R.id.nice_spinner_height);
         List<String> heightList = new LinkedList<>(Arrays.asList(heights));
         niceSpinnerHeight.attachDataSource(heightList);
+        niceSpinnerYears.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(yearList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerDegree = (NiceSpinner) findViewById(R.id.nice_spinner_degree);
         List<String> degreeList = new LinkedList<>(Arrays.asList(degrees));
         niceSpinnerDegree.attachDataSource(degreeList);
+        niceSpinnerYears.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(yearList.get(i)), Toast.LENGTH_SHORT).show();
+            }
 
+        });
+        /*
         NiceSpinner niceSpinnerRegion = (NiceSpinner) findViewById(R.id.nice_spinner_region);
         List<String> regionList = new LinkedList<>(Arrays.asList(regions));
         niceSpinnerRegion.attachDataSource(regionList);
+        niceSpinnerYears.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(yearList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerProvince = (NiceSpinner) findViewById(R.id.nice_spinner_province);
         List<String> provinceList = new LinkedList<>(Arrays.asList(provinces));
         niceSpinnerProvince.attachDataSource(provinceList);
+        niceSpinnerYears.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(yearList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerCity = (NiceSpinner) findViewById(R.id.nice_spinner_city);
         List<String> cityList = new LinkedList<>(Arrays.asList(cities));
         niceSpinnerCity.attachDataSource(cityList);
+        niceSpinnerYears.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(yearList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        */
 
         final Button preButton = (Button)findViewById(R.id.prev);
         final Button nextButton = (Button) findViewById(R.id.next);
@@ -110,35 +179,117 @@ public class FillMeetInfoActivity extends AppCompatActivity {
                 doneButton.setVisibility(View.GONE);
             }
         });
+
+        RadioGroup self_sex = (RadioGroup)findViewById(R.id.self_sex);
+        self_sex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.self_sex_male){
+                    Toast.makeText(FillMeetInfoActivity.this,"male", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(FillMeetInfoActivity.this,"female", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
     private void createRequireView(){
 
-        List<String> ageList = new LinkedList<>(Arrays.asList(ages));
+        final List<String> ageList = new LinkedList<>(Arrays.asList(ages));
         NiceSpinner niceSpinnerAgeLower = (NiceSpinner) findViewById(R.id.nice_spinner_require_age_lower);
         niceSpinnerAgeLower.attachDataSource(ageList);
+        niceSpinnerAgeLower.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(ageList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerAgeupper = (NiceSpinner) findViewById(R.id.nice_spinner_require_age_upper);
         niceSpinnerAgeupper.attachDataSource(ageList);
+        niceSpinnerAgeupper.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(ageList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerHeight = (NiceSpinner) findViewById(R.id.nice_spinner_require_height);
-        List<String> heightList = new LinkedList<>(Arrays.asList(heights));
+        final List<String> heightList = new LinkedList<>(Arrays.asList(heights));
         niceSpinnerHeight.attachDataSource(heightList);
+        niceSpinnerHeight.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(heightList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerDegree = (NiceSpinner) findViewById(R.id.nice_spinner_require_degree);
-        List<String> degreeList = new LinkedList<>(Arrays.asList(degrees));
+        final List<String> degreeList = new LinkedList<>(Arrays.asList(degrees));
         niceSpinnerDegree.attachDataSource(degreeList);
+        niceSpinnerDegree.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(degreeList.get(i)), Toast.LENGTH_SHORT).show();
+            }
 
+        });
+
+/*
         NiceSpinner niceSpinnerRegion = (NiceSpinner) findViewById(R.id.nice_spinner_require_region);
-        List<String> regionList = new LinkedList<>(Arrays.asList(regions));
+        final List<String> regionList = new LinkedList<>(Arrays.asList(regions));
         niceSpinnerRegion.attachDataSource(regionList);
+        niceSpinnerRegion.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(regionList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerProvince = (NiceSpinner) findViewById(R.id.nice_spinner_require_province);
-        List<String> provinceList = new LinkedList<>(Arrays.asList(provinces));
+        final List<String> provinceList = new LinkedList<>(Arrays.asList(provinces));
         niceSpinnerProvince.attachDataSource(provinceList);
+        niceSpinnerProvince.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(provinceList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
         NiceSpinner niceSpinnerCity = (NiceSpinner) findViewById(R.id.nice_spinner_require_city);
-        List<String> cityList = new LinkedList<>(Arrays.asList(cities));
+        final List<String> cityList = new LinkedList<>(Arrays.asList(cities));
         niceSpinnerCity.attachDataSource(cityList);
+        niceSpinnerCity.addOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //Slog.e("什么数据",String.valueOf(yearList.get(i)));
+                Toast.makeText(FillMeetInfoActivity.this, String.valueOf(cityList.get(i)), Toast.LENGTH_SHORT).show();
+            }
+
+        });
+        */
+
+        RadioGroup require_sex = (RadioGroup)findViewById(R.id.require_sex);
+        require_sex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.require_sex_male){
+                    Toast.makeText(FillMeetInfoActivity.this,"male", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(FillMeetInfoActivity.this,"female", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
