7ccee86a0bb17f01200b7c33d34c169bd4b53021
super-zou <531387914@qq.com>
2018-09-30 19:00:36 +0800

    Update InvitationDialogFragment.java

--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/util/InvitationDialogFragment.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/util/InvitationDialogFragment.java
@@ -4,21 +4,55 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.DialogFragment;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.MultiAutoCompleteTextView;
+import android.widget.SearchView;
 
 import com.tongmenhui.launchak47.R;
 
+import com.tongmenhui.launchak47.meet.MeetDynamics;
+import com.tongmenhui.launchak47.meet.MeetDynamicsFragment;
+import com.tongmenhui.launchak47.meet.MeetMemberInfo;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.Response;
+
 public class InvitationDialogFragment extends DialogFragment implements View.OnClickListener{
+    private static final String TAG = "InvitationDialogFragment";
     private Dialog mDialog;
-    private MultiAutoCompleteTextView multiAutoCompleteTextView;
-    private ArrayAdapter<String> adapter;
+    private RecyclerView searchResultsView;
+    //private ArrayAdapter<String> adapter;
+    private SearchUserListAdapter adapter;
+    private List<MeetMemberInfo> mMemberInfoList = new ArrayList<>();
     private Context mContext;
+    private SearchView mSearchView;
+    private static final int QUERY_USER_DONE = 0;
+    private static final int PAGE_SIZE = 20;
+    private static final String QUERY_USER_URL = HttpUtil.DOMAIN + "?q=personal_archive/query";
+
+    private Handler handler = new MyHandler(this);
 
     @Override
     public void onAttach(Context context) {
@@ -29,7 +63,7 @@ public class InvitationDialogFragment extends DialogFragment implements View.OnC
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
 
-        mDialog = new Dialog(getActivity(), R.style.Theme_Design_Light);
+        mDialog = new Dialog(getActivity(), android.R.style.Theme_Light_NoTitleBar_Fullscreen);
         mDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         mDialog.setContentView(R.layout.invite_reference);
         mDialog.setCanceledOnTouchOutside(true);
@@ -38,25 +72,131 @@ public class InvitationDialogFragment extends DialogFragment implements View.OnC
         //layoutParams.alpha = 0.9f;
         layoutParams.gravity = Gravity.TOP;
         layoutParams.width = WindowManager.LayoutParams.MATCH_PARENT;
-        layoutParams.height = WindowManager.LayoutParams.WRAP_CONTENT;
+        layoutParams.height = WindowManager.LayoutParams.MATCH_PARENT;
         window.setAttributes(layoutParams);
 
         multiAutoCompleteTextView = mDialog.findViewById(R.id.multiAutoCompleteTextView);
         adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_dropdown_item_1line);
 
-        adapter.add("刘");
-        adapter.add("刘易阳");
-        adapter.add("刘一方");
-        adapter.add("刘一图");
-        adapter.add("刘一一");
-        adapter.add("刘一一一");
-        adapter.add("王");
+        mSearchView = mDialog.findViewById(R.id.searchUserView);
+        searchResultsView = mDialog.findViewById(R.id.searchResultsView);
+        //adapter = new ArrayAdapter<String>(mContext, android.R.layout.simple_dropdown_item_1line);
+        adapter = new SearchUserListAdapter(mContext);
+        searchResultsView.setAdapter(adapter);
+        
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if(!query.equals("")){
+                    Slog.d(TAG, "===========submit text: "+query);
+                    searchUserResults(query, false);
+                }
+                return false;
+            }
 
-        multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
-        multiAutoCompleteTextView.setAdapter(adapter);
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                if(newText.length() > 1){
+                    Slog.d(TAG, "===========search text:"+newText);
+                    searchUserResults(newText, true);
+                }
+                return false;
+            }
+        });
 
         return mDialog;
     }
+    
+        public void searchUserResults(String word, boolean autocomplete){
+        int page = mMemberInfoList.size() / PAGE_SIZE;
+        FormBody requestBody = new FormBody.Builder()
+                                            .add("name", word)
+                                            .add("step", String.valueOf(PAGE_SIZE))
+                                            .add("page", String.valueOf(page))
+                                            .build();
+        HttpUtil.sendOkHttpRequest(mContext, QUERY_USER_URL, requestBody, new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if(response.body() != null){
+                    String responseText = response.body().string();
+                    Slog.d(TAG, "==========response text : "+responseText);
+                    if(responseText != null){
+                         List<MeetMemberInfo> memberInfos = parseSearchUser(responseText);
+                        if (null != memberInfos) {
+                            mMemberInfoList.addAll(memberInfos);
+                            Slog.d(TAG, "getResponseText list.size:"+memberInfos.size());
+                        }
+                        handler.sendEmptyMessage(QUERY_USER_DONE);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+        });
+    }
+    
+        private List<MeetMemberInfo> parseSearchUser(String response){
+        List<MeetMemberInfo> memberInfoList = new ArrayList<MeetMemberInfo>();
+        if(!TextUtils.isEmpty(response)){
+           try{
+               JSONObject memberInfosResponse = new JSONObject(response);
+               JSONArray memberInfoArray = memberInfosResponse.getJSONArray("user");
+               if(memberInfoArray.length() > 0){
+                   for (int i=0; i<memberInfoArray.length(); i++){
+                       MeetMemberInfo memberInfo = new MeetMemberInfo();
+                       JSONObject member = memberInfoArray.getJSONObject(i);
+                       memberInfo.setRealname(member.getString("realname"));
+                       String profile = "";
+                       if(member.getInt("situation") == 0){
+                           profile = member.getString("university")+"."
+                                   +member.getString("degree")+"."
+                                   +member.getString("major");
+                       }else{
+                           profile = member.getString("job_title")+"."+member.getString("company");
+                       }
+                       memberInfo.setProfile(profile);
+                       memberInfo.setPictureUri(member.getString("picture_uri"));
+
+                       memberInfoList.add(memberInfo);
+                   }
+               }
+           }catch (JSONException e){
+               e.printStackTrace();
+           }
+            }
+        return memberInfoList;
+    }
+    
+    static class MyHandler extends Handler {
+        WeakReference<InvitationDialogFragment> invitationDialogFragmentWeakReference;
+
+        MyHandler(InvitationDialogFragment invitationDialogFragment) {
+            invitationDialogFragmentWeakReference = new WeakReference<InvitationDialogFragment>(invitationDialogFragment);
+        }
+
+        @Override
+        public void handleMessage(Message message) {
+            InvitationDialogFragment invitationDialogFragment = invitationDialogFragmentWeakReference.get();
+            if(invitationDialogFragment != null){
+                invitationDialogFragment.handleMessage(message);
+            }
+        }
+    }
+    
+        public void handleMessage(Message message){
+        switch (message.what){
+            case QUERY_USER_DONE:
+                adapter.setData(mMemberInfoList);
+                adapter.notifyDataSetChanged();
+                break;
+            default:
+                break;
+        }
+    }
+  
 
     @Override
     public void onClick(View view){
