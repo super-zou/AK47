0c586d0ec0c02ff17c7a75b7c980d28dc2b7cfd3
super-zou <531387914@qq.com>
2018-11-30 18:48:33 +0800

    Update MeetImpressionStatisticsAdapter.java

--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/adapter/MeetImpressionStatisticsAdapter.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/adapter/MeetImpressionStatisticsAdapter.java
@@ -27,12 +27,13 @@ public class MeetImpressionStatisticsAdapter extends RecyclerView.Adapter<MeetIm
     private static final String TAG = "MeetImpressionStatisticsAdapter";
     private static Context mContext;
     RequestQueue queue;
+    private MyItemClickListener mItemClickListener;
     private List<ArchivesActivity.ImpressionStatistics> mImpressionStatisticsList;
     //private ApprovedUsersDialogFragment approvedUsersDialogFragment;
     private android.support.v4.app.FragmentManager mFragmentManager;
 
     public MeetImpressionStatisticsAdapter(Context context, android.support.v4.app.FragmentManager fragmentManager) {
-        Slog.d(TAG, "==============MeetImpressionStatisticsAdapter init=================");
+
         mContext = context;
         mFragmentManager = fragmentManager;
     }
@@ -45,17 +46,16 @@ public class MeetImpressionStatisticsAdapter extends RecyclerView.Adapter<MeetIm
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.impression_statistics_item, parent, false);
-        ViewHolder holder = new ViewHolder(view);
+        ViewHolder holder = new ViewHolder(view, mItemClickListener);
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MeetImpressionStatisticsAdapter.ViewHolder holder, int position) {
-        Slog.d(TAG, "========================onBindViewHolder position: " + position);
-        final ArchivesActivity.ImpressionStatistics impressionStatistics = mImpressionStatisticsList.get(position);
-        holder.feature.setText(impressionStatistics.impression);
-        holder.featureCount.setText(String.valueOf(impressionStatistics.impressionCount));
 
+        final ArchivesActivity.ImpressionStatistics impressionStatistics = mImpressionStatisticsList.get(position);
+        holder.feature.setText(impressionStatistics.impression + " · " + String.valueOf(impressionStatistics.impressionCount));
+        
         MeetMemberInfo meetMemberInfo = impressionStatistics.meetMemberList.get(0);
         if (meetMemberInfo.getPictureUri() != null && !"".equals(meetMemberInfo.getPictureUri())) {
             queue = RequestQueueSingleton.instance(mContext);
@@ -101,18 +101,9 @@ public class MeetImpressionStatisticsAdapter extends RecyclerView.Adapter<MeetIm
             holder.approvedUsers.setText("认可");
         } else {
             holder.approvedUsers.setText("等" + impressionStatistics.meetMemberList.size() + "人认可");
-            holder.details.setVisibility(View.VISIBLE);
+            
         }
 
-        holder.details.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(mContext, ApprovedUsersActivity.class);
-                intent.putExtra("impressionStatistics", impressionStatistics);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS);
-                mContext.startActivity(intent);
-            }
-        });
     }
 
     @Override
@@ -120,17 +111,16 @@ public class MeetImpressionStatisticsAdapter extends RecyclerView.Adapter<MeetIm
         return mImpressionStatisticsList != null ? mImpressionStatisticsList.size() : 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder {
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private MyItemClickListener mListener;
         TextView feature;
-        TextView featureCount;
         ImageView headPicture1;
         ImageView headPicture2;
         ImageView headPicture3;
         TextView approvedUsers;
-        TextView details;
 
 
-        public ViewHolder(View view) {
+        public ViewHolder(View view, MyItemClickListener myItemClickListener) {
             super(view);
             feature = view.findViewById(R.id.feature);
             featureCount = view.findViewById(R.id.feature_count);
@@ -139,9 +129,41 @@ public class MeetImpressionStatisticsAdapter extends RecyclerView.Adapter<MeetIm
             headPicture3 = view.findViewById(R.id.head_picure3);
             approvedUsers = view.findViewById(R.id.approved_users);
             details = view.findViewById(R.id.details);
+            
+                        //将全局的监听赋值给接口
+            this.mListener = myItemClickListener;
+            itemView.setOnClickListener(this);
 
             //Typeface font = Typeface.createFromAsset(mContext.getAssets(), "fonts/fontawesome-webfont_4.7.ttf");
             //FontManager.markAsIconContainer(view.findViewById(R.id.evaluation_item), font);
         }
+                /**
+         * 实现OnClickListener接口重写的方法
+         * @param v
+         */
+        @Override
+        public void onClick(View v) {
+            if (mListener != null) {
+                mListener.onItemClick(v, getPosition());
+            }
+
+        }
+        
+        
+    }
+        /**
+     * 创建一个回调接口
+     */
+    public interface MyItemClickListener {
+        void onItemClick(View view, int position);
+    }
+
+    /**
+     * 在activity里面adapter就是调用的这个方法,将点击事件监听传递过来,并赋值给全局的监听
+     *
+     * @param myItemClickListener
+     */
+    public void setItemClickListener(MyItemClickListener myItemClickListener) {
+        this.mItemClickListener = myItemClickListener;
     }
 }
