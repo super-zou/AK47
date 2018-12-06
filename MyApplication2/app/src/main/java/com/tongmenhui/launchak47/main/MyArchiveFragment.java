85130ac4e1a29c1cce9654322cdea697b65df36f
super-zou <531387914@qq.com>
2018-12-06 14:14:05 +0800

    Update and rename ArchiveFragment.java to MyArchiveFragment.java

--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/main/MyArchiveFragment.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/main/MyArchiveFragment.java
@@ -13,7 +13,7 @@ import com.tongmenhui.launchak47.util.BaseFragment;
  * Created by super-zou on 17-9-11.
  */
 
-public class ArchiveFragment extends BaseFragment {
+public class MyArchiveFragment extends BaseFragment {
 
     @Nullable
     @Override
