b72e2640695ef97b85357c3fdd1870969bbaae27
chunpingxu <773964842@qq.com>
2018-12-24 22:06:33 +0800

    add MI Push

--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/util/MyApplication.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/util/MyApplication.java
@@ -1,13 +1,28 @@
 package com.tongmenhui.launchak47.util;
 
+import android.app.ActivityManager;
 import android.app.Application;
 import android.content.Context;
+import android.os.Process;
+import android.util.Log;
+
+import com.xiaomi.channel.commonutils.logger.LoggerInterface;
+import com.xiaomi.mipush.sdk.Logger;
+import com.xiaomi.mipush.sdk.MiPushClient;
+
+import java.util.List;
 
 /**
  * Created by super-zou on 18-1-20.
  */
 
 public class MyApplication extends Application {
+    //+Begin added by xuchunping for MI push
+    private static final String TAG = "MyApplication";
+    public static final String APP_ID = "2882303761517916663";
+    public static final String APP_KEY = "5261791687663";
+    //-End added by xuchunping for MI push
+
     private static Context mContext;
 
     public static Context getContext() {
@@ -17,5 +32,41 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         mContext = getApplicationContext();
+
+        //+Begin added by xuchunping for MI push
+        if(shouldInit()) {
+            MiPushClient.registerPush(this, APP_ID, APP_KEY);
+        }
+        LoggerInterface newLogger = new LoggerInterface() {
+            @Override
+            public void setTag(String tag) {
+                // ignore
+            }
+            @Override
+            public void log(String content, Throwable t) {
+                Log.d(TAG, content, t);
+            }
+            @Override
+            public void log(String content) {
+                Log.d(TAG, content);
+            }
+        };
+        Logger.setLogger(this, newLogger);
+        //-End added by xuchunping for MI push
     }
+
+    //+Begin added by xuchunping for MI push
+    private boolean shouldInit() {
+        ActivityManager am = ((ActivityManager) getSystemService(Context.ACTIVITY_SERVICE));
+        List<ActivityManager.RunningAppProcessInfo> processInfos = am.getRunningAppProcesses();
+        String mainProcessName = getPackageName();
+        int myPid = Process.myPid();
+        for (ActivityManager.RunningAppProcessInfo info : processInfos) {
+            if (info.pid == myPid && mainProcessName.equals(info.processName)) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //-End added by xuchunping for MI push
 }
