43bf6194f8e5e412c879644e21c27f667a81948f
super-zou <531387914@qq.com>
2019-01-23 15:30:47 +0800

    Update Login.java

--- MyApplication2/app/src/main/java/com/tongmenhui/launchak47/join/Login.java
+++ MyApplication2/app/src/main/java/com/tongmenhui/launchak47/join/Login.java
@@ -29,6 +29,7 @@ import okhttp3.Callback;
 import okhttp3.FormBody;
 import okhttp3.RequestBody;
 import okhttp3.Response;
+import com.tongmenhui.launchak47.R;
 
 public class Login extends AppCompatActivity {
     private static final String TAG = "Login";
