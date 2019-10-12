e4ca5453b99b0ca3642199cd7a1e28b204ca1ce6
super-zou <531387914@qq.com>
2019-10-12 18:16:08 +0800

    Update EvaluatorDetailsAdapter.java

--- MyApplication2/app/src/main/java/com/hetang/adapter/EvaluatorDetailsAdapter.java
+++ MyApplication2/app/src/main/java/com/hetang/adapter/EvaluatorDetailsAdapter.java
@@ -10,32 +10,29 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.android.volley.RequestQueue;
-import com.android.volley.toolbox.NetworkImageView;
+import com.bumptech.glide.Glide;
+import com.hetang.common.MyApplication;
 import com.nex3z.flowlayout.FlowLayout;
 import com.hetang.R;
 import com.hetang.meet.EvaluatorDetailsActivity;
 import com.hetang.util.HttpUtil;
-import com.hetang.util.RequestQueueSingleton;
-import com.hetang.util.Slog;
+import com.hetang.util.ParseUtils;
+import com.hetang.util.RoundImageView;
 import com.willy.ratingbar.ScaleRatingBar;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.hetang.util.ParseUtils.getMeetArchive;
+import static com.hetang.util.ParseUtils.startMeetArchiveActivity;
 
 public class EvaluatorDetailsAdapter extends RecyclerView.Adapter<EvaluatorDetailsAdapter.ViewHolder> {
     private static final String TAG = "EvaluatorDetailsAdapter";
-    RequestQueue queueEvaluator;
-    RequestQueueSingleton requestQueueSingleton;
     private Context context;
     private List<EvaluatorDetailsActivity.EvaluatorDetails> mEvaluatorDetailsList;
 
     public EvaluatorDetailsAdapter(Context context) {
         this.context = context;
         mEvaluatorDetailsList = new ArrayList<>();
-        requestQueueSingleton = new RequestQueueSingleton();
     }
 
     public void setData(List<EvaluatorDetailsActivity.EvaluatorDetails> evaluatorDetailsList) {
@@ -53,35 +50,61 @@ public class EvaluatorDetailsAdapter extends RecyclerView.Adapter<EvaluatorDetai
     @Override
     public void onBindViewHolder(@NonNull final EvaluatorDetailsAdapter.ViewHolder holder, int position) {
         final EvaluatorDetailsActivity.EvaluatorDetails evaluatorDetails = mEvaluatorDetailsList.get(position);
-        holder.headUri.setTag(HttpUtil.DOMAIN + "/" + evaluatorDetails.getPictureUri());
-        queueEvaluator = requestQueueSingleton.instance(context);
-        HttpUtil.loadByImageLoader(queueEvaluator, holder.headUri, HttpUtil.DOMAIN + "/" + evaluatorDetails.getPictureUri(), 50, 50);
-
+        String avatar = evaluatorDetails.getAvatar();
+        if (avatar != null && !"".equals(avatar)) {
+            Glide.with(context).load(HttpUtil.DOMAIN + avatar).into(holder.avatar);
+        } else {
+            if(evaluatorDetails.getSex() == 0){
+                holder.avatar.setImageDrawable(MyApplication.getContext().getDrawable(R.drawable.male_default_avator));
+            }else {
+                holder.avatar.setImageDrawable(MyApplication.getContext().getDrawable(R.drawable.female_default_avator));
+            }
+        }
+        
         holder.name.setText(evaluatorDetails.getName());
         holder.uid.setText(String.valueOf(evaluatorDetails.getEvaluatorUid()));
         holder.scaleRatingBar.setRating((float) evaluatorDetails.getRating());
         holder.rating.setText(evaluatorDetails.getRating() + "分");
-        Slog.d(TAG, "=====================getImpression: " + evaluatorDetails.getFeatures());
-        if (evaluatorDetails.getFeatures() != null && !"".equals(evaluatorDetails.getFeatures())) {
-            String[] featureArray = evaluatorDetails.getFeatures().split("#");
-            holder.features.removeAllViews();
-            for (int i = 0; i < featureArray.length; i++) {
-                TextView diyTextView = new TextView(context);
-                diyTextView.setPadding((int) dpToPx(8), (int) dpToPx(8), (int) dpToPx(8), (int) dpToPx(8));
-                diyTextView.setText(featureArray[i]);
-                diyTextView.setGravity(Gravity.CENTER);
-                diyTextView.setBackground(context.getDrawable(R.drawable.label_bg));
-                holder.features.addView(diyTextView);
+        //Slog.d(TAG, "=====================getImpression: " + evaluatorDetails.getFeatures());
+
+        String features = evaluatorDetails.getFeatures();
+        
+        if (!"".equals(features) && !"null".equals(features)) {
+            if(holder.features.getTag() == null){
+                setFeatureView(holder.features, evaluatorDetails, features);
+            }else {
+                if(!features.equals(holder.features.getTag())){
+                    holder.features.removeAllViews();
+                    setFeatureView(holder.features, evaluatorDetails, features);
+                }
+            }
+        }else {
+            if (holder.features.getChildCount() > 0){
+                holder.features.removeAllViews();
             }
         }
-        holder.headUri.setOnClickListener(new View.OnClickListener() {
+        
+        holder.avatar.setOnClickListener(new View.OnClickListener() {
             int uid = Integer.parseInt(holder.uid.getText().toString());
-
             @Override
             public void onClick(View view) {
-                getMeetArchive(context, uid);
+                ParseUtils.startArchiveActivity(context, uid);
             }
         });
+    }
+    
+    private void setFeatureView(FlowLayout featureView, EvaluatorDetailsActivity.EvaluatorDetails evaluatorDetails, String features){
+        String[] featureArray = features.split("#");
+
+        for (int i = 0; i < featureArray.length; i++) {
+            TextView diyTextView = new TextView(context);
+            diyTextView.setPadding((int) dpToPx(8), (int) dpToPx(8), (int) dpToPx(8), (int) dpToPx(8));
+            diyTextView.setText(featureArray[i]);
+            diyTextView.setGravity(Gravity.CENTER);
+            diyTextView.setBackground(context.getDrawable(R.drawable.label_bg));
+            featureView.addView(diyTextView);
+        }
+        featureView.setTag(evaluatorDetails);
     }
 
     @Override
@@ -94,7 +117,7 @@ public class EvaluatorDetailsAdapter extends RecyclerView.Adapter<EvaluatorDetai
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        NetworkImageView headUri;
+        RoundImageView avatar;
         TextView name;
         TextView uid;
         ScaleRatingBar scaleRatingBar;
@@ -103,7 +126,7 @@ public class EvaluatorDetailsAdapter extends RecyclerView.Adapter<EvaluatorDetai
 
         public ViewHolder(View view) {
             super(view);
-            headUri = view.findViewById(R.id.evaluator_picture);
+            avatar = view.findViewById(R.id.evaluator_picture);
             name = view.findViewById(R.id.evaluator_name);
             uid = view.findViewById(R.id.uid);
             scaleRatingBar = view.findViewById(R.id.charm_rating);
