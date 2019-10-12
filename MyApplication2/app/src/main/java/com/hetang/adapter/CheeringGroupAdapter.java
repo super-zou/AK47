48ed779c5a7da8633d0fe9a619e3504b23126633
super-zou <531387914@qq.com>
2019-10-12 18:12:27 +0800

    Update CheeringGroupAdapter.java

--- MyApplication2/app/src/main/java/com/hetang/adapter/CheeringGroupAdapter.java
+++ MyApplication2/app/src/main/java/com/hetang/adapter/CheeringGroupAdapter.java
@@ -1,94 +1,106 @@
 package com.hetang.adapter;
 
 import android.content.Context;
-import android.support.annotation.NonNull;
-import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.TextView;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 
-import com.android.volley.RequestQueue;
 import com.bumptech.glide.Glide;
 import com.hetang.R;
-import com.hetang.meet.MeetMemberInfo;
 import com.hetang.util.HttpUtil;
-import com.hetang.util.Slog;
+import com.hetang.common.MyApplication;
+import com.hetang.util.RoundImageView;
+import com.hetang.util.UserProfile;
+import com.hetang.util.Utility;
 
 import java.util.List;
 
-public class CheeringGroupAdapter extends RecyclerView.Adapter<CheeringGroupAdapter.ViewHolder>{
+public class CheeringGroupAdapter extends BaseAdapter {
     private static final String TAG = "CheeringGroupAdapter";
     private static Context mContext;
-    RequestQueue queue;
-    private List<MeetMemberInfo> mCheeringGroupList;
-        private MyItemClickListener mItemClickListener;
+    int width;
+    private List<UserProfile> mCheeringGroupList;
 
-    public CheeringGroupAdapter(Context context) {
+    public CheeringGroupAdapter(Context context, int width) {
         mContext = context;
+        this.width = width - (int)Utility.dpToPx(mContext, 4f);
+        //this.width = width;
     }
     
-        public void setCheeringGroupList(List<MeetMemberInfo> cheeringGroupList) {
+    public void setCheeringGroupList(List<UserProfile> cheeringGroupList) {
         mCheeringGroupList = cheeringGroupList;
-    }
-
-    @Override
-    public CheeringGroupAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.cheering_group_item, parent, false);
-        CheeringGroupAdapter.ViewHolder holder = new CheeringGroupAdapter.ViewHolder(view, mItemClickListener);
-        return holder;
     }
     
         @Override
-    public void onBindViewHolder(@NonNull CheeringGroupAdapter.ViewHolder holder, int position) {
-        final MeetMemberInfo cheeringGroup = mCheeringGroupList.get(position);
-        holder.realName.setText(cheeringGroup.getRealname());
-        //holder.profile.setText(cheeringGroup.getProfile());
-        
+    public View getView(int position, View convertView, ViewGroup parent){
+        ViewHolder viewHolder;
+        if (convertView == null){
+            convertView = View.inflate(mContext, R.layout.cheering_group_item, null);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder)convertView.getTag();
+        }
+
+        final UserProfile cheeringGroup = mCheeringGroupList.get(position);
+        setViewHolder(viewHolder, cheeringGroup);
+        return convertView;
+    }
+    
+        private void setViewHolder(ViewHolder holder, UserProfile cheeringGroup){
+        holder.name.setText(cheeringGroup.getName());
+        //holder.profile.setText(cheeringGroup.getBaseProfile());
+
         if(cheeringGroup.getSituation() == 0){
-            Slog.d(TAG, "===============get degree: "+cheeringGroup.getDegree()+"   uid: "+cheeringGroup.getUid());
-            holder.degree.setText(cheeringGroup.getDegree());
+            holder.degree.setText(cheeringGroup.getDegreeName(cheeringGroup.getDegree()));
             holder.university.setText(cheeringGroup.getUniversity());
         }else {
             holder.education.setVisibility(View.GONE);
             holder.work.setVisibility(View.VISIBLE);
-            holder.title.setText(cheeringGroup.getJobTitle());
-            holder.company.setText(cheeringGroup.getCompany());
+            holder.title.setText(cheeringGroup.getPosition());
+            holder.company.setText(cheeringGroup.getIndustry());
         }
-
-        if (cheeringGroup.getPictureUri() != null && !"".equals(cheeringGroup.getPictureUri())) {
-            Glide.with(mContext).load(HttpUtil.DOMAIN + cheeringGroup.getPictureUri()).into(holder.headUri);
-            //queue = RequestQueueSingleton.instance(mContext);
-            //holder.headUri.setTag(HttpUtil.DOMAIN + cheeringGroup.getPictureUri());
-            //HttpUtil.loadByImageLoader(queue, holder.headUri, HttpUtil.DOMAIN + cheeringGroup.getPictureUri(), 37, 60);
+String avatar = cheeringGroup.getAvatar();
+        if (avatar != null && !"".equals(avatar)) {
+            Glide.with(mContext).load(HttpUtil.DOMAIN + cheeringGroup.getAvatar()).into(holder.headUri);
         } else {
-            holder.headUri.setImageDrawable(mContext.getDrawable(R.mipmap.ic_launcher));
+            if(cheeringGroup.getSex() == 0){
+                holder.headUri.setImageDrawable(MyApplication.getContext().getDrawable(R.drawable.male_default_avator));
+            }else {
+                holder.headUri.setImageDrawable(MyApplication.getContext().getDrawable(R.drawable.female_default_avator));
+            }
         }
-
+        ViewGroup.LayoutParams lp = holder.headUri.getLayoutParams();
+        lp.width = width/2;
+        lp.height = lp.width;
+        holder.headUri.setLayoutParams(lp);
     }
     
         @Override
-    public int getItemCount() {
+    public int getCount() {
         return mCheeringGroupList != null ? mCheeringGroupList.size() : 0;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        private MyItemClickListener mListener;
-        TextView realName;
+    @Override
+    public Object getItem(int position){ return  mCheeringGroupList.get(position); }
+
+    @Override
+    public long getItemId(int position){ return position; }
+    
+    public class ViewHolder{
+        TextView name;
         TextView degree;
         TextView university;
         TextView title;
         TextView company;
         LinearLayout education;
         LinearLayout work;
-        ImageView headUri;
-
-        public ViewHolder(View view, MyItemClickListener myItemClickListener) {
-            super(view);
-            realName = view.findViewById(R.id.name);
+        RoundImageView headUri;
+        
+        public ViewHolder(View view) {
+            name = view.findViewById(R.id.name);
             headUri = view.findViewById(R.id.head_uri);
             education = view.findViewById(R.id.education);
             degree = view.findViewById(R.id.degree);
@@ -96,38 +108,7 @@ public class CheeringGroupAdapter extends RecyclerView.Adapter<CheeringGroupAdap
             work = view.findViewById(R.id.work);
             title = view.findViewById(R.id.title);
             company = view.findViewById(R.id.company);
-
-            //将全局的监听赋值给接口
-            this.mListener = myItemClickListener;
-            itemView.setOnClickListener(this);
         }
         
-         /**
-         * 实现OnClickListener接口重写的方法
-         * @param v
-         */
-        @Override
-        public void onClick(View v) {
-            if (mListener != null) {
-                mListener.onItemClick(v, getPosition());
-            }
-
-        }
-    }
-    
-     /**
-     * 创建一个回调接口
-     */
-    public interface MyItemClickListener {
-        void onItemClick(View view, int position);
-    }
-
-    /**
-     * 在activity里面adapter就是调用的这个方法,将点击事件监听传递过来,并赋值给全局的监听
-     *
-     * @param myItemClickListener
-     */
-    public void setItemClickListener(CheeringGroupAdapter.MyItemClickListener myItemClickListener) {
-        this.mItemClickListener = myItemClickListener;
-    }
-}
+      }
+ }
