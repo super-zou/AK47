2d95522d08b0c713bd32462dfe5bd5a5a3dbe295
super-zou <531387914@qq.com>
2019-10-15 19:05:33 +0800

    Update EducationEditDialogFragment.java

--- MyApplication2/app/src/main/java/com/hetang/archive/EducationEditDialogFragment.java
+++ MyApplication2/app/src/main/java/com/hetang/archive/EducationEditDialogFragment.java
@@ -3,6 +3,7 @@ package com.hetang.archive;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.design.widget.TextInputEditText;
