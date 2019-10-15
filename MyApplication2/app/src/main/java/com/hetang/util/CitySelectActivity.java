599baa35f30464230571b5933847f8079acbf4bc
super-zou <531387914@qq.com>
2019-10-15 11:47:07 +0800

    Create CitySelectActivity.java

---
+++ MyApplication2/app/src/main/java/com/hetang/util/CitySelectActivity.java
@@ -0,0 +1,15 @@
+package com.hetang.util;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hetang.R;
+
+public class CitySelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_select);
+    }
+}
