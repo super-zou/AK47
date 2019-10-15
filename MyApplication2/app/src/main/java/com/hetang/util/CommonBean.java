00687ea83427b3f8d0060496f6328180c9ac54b3
super-zou <531387914@qq.com>
2019-10-15 11:47:27 +0800

    Create CommonBean.java

---
+++ MyApplication2/app/src/main/java/com/hetang/util/CommonBean.java
@@ -0,0 +1,39 @@
+package com.hetang.util;
+
+
+import com.contrarywind.interfaces.IPickerViewData;
+
+import java.util.List;
+
+public class CommonBean implements IPickerViewData {
+
+    /**
+     * mainItem : 高新科技
+     * subItemList : ["互联网", "电子商务"]
+     */
+
+    private String mainItem;
+    private List<String> subItemList;
+
+    public String getMainItem() {
+        return mainItem;
+    }
+
+    public void setMainItem(String mainItem) {
+        this.mainItem = mainItem;
+    }
+
+    public List<String> getSubItemList() {
+        return subItemList;
+    }
+
+    public void setSubItemList(List<String> subItemList) {
+        this.subItemList = subItemList;
+    }
+
+    @Override
+    public String getPickerViewText() {
+        return this.mainItem;
+    }
+}
+
