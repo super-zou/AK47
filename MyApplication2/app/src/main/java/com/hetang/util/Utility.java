1ad129ed65a7cf1b05d2daff570246764a00907a
super-zou <531387914@qq.com>
2019-10-18 15:11:06 +0800

    Update Utility.java

--- MyApplication2/app/src/main/java/com/hetang/util/Utility.java
+++ MyApplication2/app/src/main/java/com/hetang/util/Utility.java
@@ -116,6 +116,18 @@ public class Utility {
         return "";
     }
     
+     /**
+     * 判断当前应用是否是debug状态
+     */
+    public static boolean isApkInDebug(Context context) {
+        try {
+            ApplicationInfo info = context.getApplicationInfo();
+            return (info.flags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
     /**
      * Drawable转换成一个Bitmap
      *
