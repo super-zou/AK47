b9940425ff21164e89f8403b788824a45871fcb6
super-zou <531387914@qq.com>
2019-11-05 09:26:04 +0800

    Update SetAvatarActivity.java

--- MyApplication2/app/src/main/java/com/hetang/util/SetAvatarActivity.java
+++ MyApplication2/app/src/main/java/com/hetang/util/SetAvatarActivity.java
@@ -10,6 +10,7 @@ import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
 import android.util.Log;
