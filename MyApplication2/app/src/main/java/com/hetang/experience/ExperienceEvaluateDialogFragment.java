beeb148aa17c32ec7c1d37b46e6ac364a483f2bd
super-zou <531387914@qq.com>
2020-04-30 18:21:08 +0800

    Update ExperienceEvaluateDialogFragment.java

--- MyApplication2/app/src/main/java/com/hetang/experience/ExperienceEvaluateDialogFragment.java
+++ MyApplication2/app/src/main/java/com/hetang/experience/ExperienceEvaluateDialogFragment.java
@@ -6,9 +6,11 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,6 +20,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.fragment.app.DialogFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -27,12 +30,14 @@ import com.bumptech.glide.Glide;
 import com.hetang.R;
 import com.hetang.adapter.GridImageAdapter;
 import com.hetang.common.HandlerTemp;
+import com.hetang.common.MyApplication;
 import com.hetang.common.OnItemClickListener;
 import com.hetang.dynamics.AddDynamicsActivity;
 import com.hetang.main.FullyGridLayoutManager;
 import com.hetang.picture.GlideEngine;
 import com.hetang.util.BaseDialogFragment;
 import com.hetang.util.CommonDialogFragmentInterface;
+import com.hetang.util.FontManager;
 import com.hetang.util.HttpUtil;
 import com.hetang.util.Slog;
 import com.luck.picture.lib.PictureSelector;
@@ -43,10 +48,15 @@ import com.luck.picture.lib.style.PictureWindowAnimationStyle;
 import com.willy.ratingbar.BaseRatingBar;
 import com.willy.ratingbar.ScaleRatingBar;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -60,6 +70,7 @@ import static com.hetang.group.GroupFragment.eden_group;
 public class ExperienceEvaluateDialogFragment extends BaseDialogFragment {
     public final static int SET_EVALUATE_RESULT_OK = 7;
     private static final String TAG = "ExperienceEvaluateDialogFragment";
+        private static final int IMPRESSION_PARSE_DONE = 0;
     private static final String WRITE_EVALUATION_URL = HttpUtil.DOMAIN + "?q=travel_guide/write_evaluate";
     private static final int WRITE_EVALUATE_DONE = 0;
     final List<String> selectedFeatures = new ArrayList<>();
@@ -116,57 +127,26 @@ public class ExperienceEvaluateDialogFragment extends BaseDialogFragment {
         window.setDimAmount(0.8f);
         window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
         window.setAttributes(layoutParams);
+        
+         TextView back = mDialog.findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mDialog.dismiss();
+            }
+        });
 
-        setImpressions();
+        setEvaluationPictureWidget();
 
-        setEvaluationPicture();
+        setEvaluation();
+
+        Typeface font = Typeface.createFromAsset(MyApplication.getContext().getAssets(), "fonts/fontawesome-webfont_4.7.ttf");
+        FontManager.markAsIconContainer(mDialog.findViewById(R.id.custom_actionbar), font);
 
         return mDialog;
     }
-    
-    private void setImpressions() {
-        ImageView headPicture = view.findViewById(R.id.head_picture);
-        Glide.with(getContext()).load(HttpUtil.DOMAIN + order.headPictureUrl).into(headPicture);
-        TextView titleTV = view.findViewById(R.id.guide_title);
-        titleTV.setText(order.title);
-
-        ScaleRatingBar scaleRatingBar = view.findViewById(R.id.charm_rating_bar);
-        final TextView charmRating = view.findViewById(R.id.charm_rating);
-        
-        scaleRatingBar.setOnRatingChangeListener(new BaseRatingBar.OnRatingChangeListener() {
-
-            @Override
-            public void onRatingChange(BaseRatingBar ratingBar, float rating) {
-                Slog.d(TAG, "onRatingChange float: " + rating);
-                charmRating.setText(String.valueOf(rating));
-            }
-
-        });
-
-        Button saveEvaluate = view.findViewById(R.id.save_evaluate);
-        final TextView errorNotice = view.findViewById(R.id.error_notice);
-        final EditText contentET = view.findViewById(R.id.content);
-        
-        saveEvaluate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                float rating = 0;
-
-                if (charmRating.getText().toString() != null && !"".equals(charmRating.getText().toString())) {
-                    rating = Float.parseFloat(charmRating.getText().toString());
-                    mEvaluated = true;
-                } else {
-                    errorNotice.setVisibility(View.VISIBLE);
-                    return;
-                }
-
-                String content = contentET.getText().toString();
-                uploadToServer(content, rating);
-            }
-        });
-    }
-    
-    private void setEvaluationPicture(){
+       
+    private void setEvaluationPictureWidget(){
         recyclerView = mDialog.findViewById(R.id.add_evaluation_picture);
         FullyGridLayoutManager manager = new FullyGridLayoutManager(getContext(), 3, GridLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(manager);
@@ -202,32 +182,118 @@ public class ExperienceEvaluateDialogFragment extends BaseDialogFragment {
         });
     }
     
-    private void uploadToServer(String content, float rating) {
-        showProgress(mContext);
-        FormBody.Builder builder = new FormBody.Builder()
-                .add("tid", String.valueOf(order.id))
-                .add("content", content)
-                .add("rating", String.valueOf(rating));
+        private void setEvaluation() {
+        ImageView headPicture = view.findViewById(R.id.head_picture);
+        Glide.with(getContext()).load(HttpUtil.DOMAIN + order.headPictureUrl).into(headPicture);
+        TextView titleTV = view.findViewById(R.id.guide_title);
+        titleTV.setText(order.title);
 
+        ScaleRatingBar scaleRatingBar = view.findViewById(R.id.charm_rating_bar);
+        final TextView charmRating = view.findViewById(R.id.charm_rating);
 
-        RequestBody requestBody = builder.build();
-        
-        HttpUtil.sendOkHttpRequest(getContext(), WRITE_EVALUATION_URL, requestBody, new Callback() {
+        scaleRatingBar.setOnRatingChangeListener(new BaseRatingBar.OnRatingChangeListener() {
+
+            @Override
+            public void onRatingChange(BaseRatingBar ratingBar, float rating) {
+                charmRating.setText(String.valueOf(rating));
+            }
+
+        });
+            Button saveEvaluate = view.findViewById(R.id.save_evaluate);
+        final EditText contentET = view.findViewById(R.id.content);
+
+        saveEvaluate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float rating = 0;
+
+                if (charmRating.getText().toString() != null && !"".equals(charmRating.getText().toString())) {
+                    rating = Float.parseFloat(charmRating.getText().toString());
+                    mEvaluated = true;
+                } else {
+                    Toast.makeText(getContext(), "请给此次体验评分", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                
+                 String content = contentET.getText().toString();
+                if (TextUtils.isEmpty(content)){
+                    Toast.makeText(getContext(), "您对此次体验有什么感受？", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                submitEvaluate(rating, content);
+            }
+        });
+            
+            headPicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), GuideDetailActivity.class);
+                intent.putExtra("tid", order.id);
+                startActivity(intent);
+            }
+        });
+        titleTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                headPicture.callOnClick();
+            }
+        });
+    }
+    
+    private void submitEvaluate(float rating, String content) {
+        showProgressDialog(getContext().getString(R.string.saving_progress));
+        Map<String, String> evaluateMap = new HashMap<>();
+
+        evaluateMap.put("oid", String.valueOf(order.oid));
+        evaluateMap.put("tid", String.valueOf(order.id));
+        evaluateMap.put("rating", String.valueOf(rating));
+        evaluateMap.put("content", content);
+        if (selectList.size() > 0) {
+            Slog.d(TAG, "----------------->submitRoute selectList: " + selectList);
+            for (LocalMedia media : selectList) {
+                selectFileList.add(new File(media.getCompressPath()));
+            }
+        }
+
+        uploadPictures(evaluateMap, "evaluate", selectFileList);
+
+    }
+    
+    
+private void uploadPictures(Map<String, String> params, String picKey, List<File> files) {
+        Slog.d(TAG, "--------------------->uploadPictures file size: " + files.size());
+        String uri = WRITE_EVALUATION_URL;
+
+        HttpUtil.uploadPictureHttpRequest(getContext(), params, picKey, files, uri, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-
                 if (response.body() != null) {
-                    String responseText = response.body().string();
-                    Slog.d(TAG, "==========loadReferences response text : " + responseText);
-                    if (responseText != null) {
-                        mEvaluated = true;
-                        handler.sendEmptyMessage(WRITE_EVALUATE_DONE);
+                    try {
+                        String responseText = response.body().string();
+                        Slog.d(TAG, "---------------->uploadPictures response: " + responseText);
+                        int result = new JSONObject(responseText).optInt("result");
+                        if (result == 1) {
+                            dismissProgressDialog();
+                            selectList.clear();
+                            selectFileList.clear();
+                            //PictureFileUtils.deleteAllCacheDirFile(MyApplication.getContext());
+                            handler.sendEmptyMessage(WRITE_EVALUATE_DONE);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
                 }
             }
-
+            
             @Override
             public void onFailure(Call call, IOException e) {
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(getContext(), getContext().getResources().getString(R.string.submit_error), Toast.LENGTH_SHORT).show();
+                    }
+                });
+                e.printStackTrace();
 
             }
         });
