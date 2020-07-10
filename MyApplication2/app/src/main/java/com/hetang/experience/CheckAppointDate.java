607083e734487d334751c02ee496308aaea1692b
super-zou <531387914@qq.com>
2020-07-10 11:57:08 +0800

    Update CheckAppointDate.java

--- MyApplication2/app/src/main/java/com/hetang/experience/CheckAppointDate.java
+++ MyApplication2/app/src/main/java/com/hetang/experience/CheckAppointDate.java
@@ -12,54 +12,39 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
-import android.widget.CompoundButton;
-import android.widget.EditText;
+
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.hetang.R;
-import com.hetang.adapter.GridImageAdapter;
 import com.hetang.common.MyApplication;
-import com.hetang.common.OnItemClickListener;
-import com.hetang.dynamics.AddDynamicsActivity;
-import com.hetang.main.FullyGridLayoutManager;
-import com.hetang.picture.GlideEngine;
+import com.hetang.home.CommonContactsActivity;
+import com.hetang.order.PlaceOrderDF;
 import com.hetang.util.BaseDialogFragment;
 import com.hetang.util.FontManager;
 import com.hetang.util.HttpUtil;
 import com.hetang.util.Slog;
-import com.luck.picture.lib.PictureSelector;
-import com.luck.picture.lib.config.PictureConfig;
-import com.luck.picture.lib.config.PictureMimeType;
-import com.luck.picture.lib.entity.LocalMedia;
-import com.luck.picture.lib.style.PictureWindowAnimationStyle;
+
+import com.hetang.util.Utility;
 import com.prolificinteractive.materialcalendarview.CalendarDay;
 import com.prolificinteractive.materialcalendarview.DayViewDecorator;
 import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.prolificinteractive.materialcalendarview.OnDateLongClickListener;
 import com.prolificinteractive.materialcalendarview.OnDateSelectedListener;
-import com.prolificinteractive.materialcalendarview.spans.DotSpan;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.threeten.bp.LocalDate;
-import org.threeten.bp.LocalDateTime;
 import org.threeten.bp.format.DateTimeFormatter;
-import java.io.File;
+
 import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -68,41 +53,58 @@ import okhttp3.RequestBody;
 import okhttp3.Response;
 
 import static android.app.Activity.RESULT_OK;
-import static com.hetang.common.MyApplication.getContext;
-import static com.hetang.experience.GuideAuthenticationDialogFragment.EXPERIENCE_TYPE_GUIDE;
-import static com.hetang.experience.GuideAuthenticationDialogFragment.MODIFY_ROUTE_INFO_URL;
-import static com.hetang.experience.GuideAuthenticationDialogFragment.ROUTE_REQUEST_CODE;
-import static com.hetang.experience.GuideAuthenticationDialogFragment.SUBMIT_ROUTE_INFO_URL;
-import static com.hetang.experience.GuideAuthenticationDialogFragment.WRITE_ROUTE_INFO_SUCCESS;
+import static com.hetang.experience.DevelopExperienceDialogFragment.FORMATTER;
+import static com.hetang.home.CommonContactsActivity.EXPERIENCE_COMPANION;
+import static com.hetang.util.DateUtil.timeStampToDay;
 
 public class CheckAppointDate extends BaseDialogFragment implements OnDateSelectedListener , OnDateLongClickListener{
     private static final boolean isDebug = true;
     private static final String TAG = "CheckAppointDate";
     private Dialog mDialog;
     private Window window;
-    private int tid;
-        private int did;
-    private int amount;
+    private static int mType = Utility.TalentType.EXPERIENCE.ordinal();
+    private int sid;
+    private int eid;
+    private int did;
+    private int price;
+    private int count = 0;
     private String unit;
+    private String title;
+    private String dataStr;
     private Button selectBtn;
     private MyHandler myHandler;
     MaterialCalendarView widget;
+    private ConstraintLayout selectWrapper;
+    private TextView companionsTV;
     private JSONArray dateJSONArray;
-        private ConstraintLayout selectWrapper;
     private static CalendarDay today;
-    private static final int GET_AVAILABLE_APPOINTMENT_DATE_DONE = 1;
-        private static List<LocalDate> availableDateList = new ArrayList<>();
+    private static List<LocalDate> availableDateList = new ArrayList<>();
     private static List<AppointDate> appointDateList = new ArrayList<>();
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("EEE, d MMM yyyy");
+    private static final int GET_AVAILABLE_APPOINTMENT_DATE_DONE = 1;
     public static final String GET_AVAILABLE_APPOINTMENT_DATE = HttpUtil.DOMAIN + "?q=travel_guide/get_available_appointment_date";
+    public static final String GET_EXPERIENCE_AVAILABLE_APPOINTMENT_DATE = HttpUtil.DOMAIN + "?q=experience/get_experience_available_appointment_date";
     public static final String PLACE_ORDER = HttpUtil.DOMAIN + "?q=order_manager/place_order";
     
-    public static CheckAppointDate newInstance(int tid, int amount, String unit) {
+    public static CheckAppointDate newInstance(int sid, int price, String unit, String title) {
+        mType = Utility.TalentType.GUIDE.ordinal();
         CheckAppointDate checkAppointDate = new CheckAppointDate();
         Bundle bundle = new Bundle();
-        bundle.putInt("tid", tid);
-                bundle.putInt("amount", amount);
+        bundle.putInt("sid", sid);
+        bundle.putInt("price", price);
         bundle.putString("unit", unit);
+        bundle.putString("title", title);
+        checkAppointDate.setArguments(bundle);
+
+        return checkAppointDate;
+    }
+    
+    public static CheckAppointDate newInstance(int eid, int price, String title) {
+        mType = Utility.TalentType.EXPERIENCE.ordinal();
+        CheckAppointDate checkAppointDate = new CheckAppointDate();
+        Bundle bundle = new Bundle();
+        bundle.putInt("eid", eid);
+        bundle.putInt("price", price);
+        bundle.putString("title", title);
         checkAppointDate.setArguments(bundle);
 
         return checkAppointDate;
@@ -116,9 +118,15 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
         myHandler = new MyHandler(this);
         Bundle bundle = getArguments();
         if (bundle != null) {
-            tid = bundle.getInt("tid");
-                        amount = bundle.getInt("amount");
-            unit = bundle.getString("unit");
+            price = bundle.getInt("price");
+            title = bundle.getString("title");
+            if (mType == Utility.TalentType.EXPERIENCE.ordinal()){
+                eid = bundle.getInt("eid");
+                unit = "人起";
+            }else {
+                sid = bundle.getInt("sid");
+                unit = bundle.getString("unit");
+            }
         }
         
         mDialog.setCanceledOnTouchOutside(true);
@@ -142,11 +150,12 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
         
         TextView amountTV = mDialog.findViewById(R.id.amount);
         TextView unitTV = mDialog.findViewById(R.id.unit);
-        amountTV.setText(String.valueOf(amount));
+        amountTV.setText(String.valueOf(price));
         unitTV.setText(unit);
 
         selectWrapper = mDialog.findViewById(R.id.select_wrapper);
         selectBtn = mDialog.findViewById(R.id.select);
+        companionsTV = mDialog.findViewById(R.id.numberOfcompanions);
 
         selectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -162,36 +171,22 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
         return mDialog;
     }
     
-        private void submitOrder(){
-        showProgressDialog(getContext().getString(R.string.saving_progress));
-        RequestBody requestBody = new FormBody.Builder()
-                .add("tid", String.valueOf(tid))
-                .add("did", String.valueOf(did))
-                .build();
-            HttpUtil.sendOkHttpRequest(getContext(), PLACE_ORDER, requestBody, new Callback() {
-            @Override
-            public void onResponse(Call call, Response response) throws IOException {
-                String responseText = response.body().string();
-                if (isDebug)
-                    Slog.d(TAG, "==========submitOrder response body : " + responseText);
-                if (responseText != null) {
-                    try {
-                        dismissProgressDialog();
-                        JSONObject jsonObject = new JSONObject(responseText);
-                        int result = jsonObject.optInt("result");
-                        //processResponse();
-                        //myHandler.sendEmptyMessage(GET_AVAILABLE_APPOINTMENT_DATE_DONE);
-                    }catch (JSONException e){
-                        e.printStackTrace();
-                    }
-                }
-            }
-                
-                @Override
-            public void onFailure(Call call, IOException e) {
+    private void getCompanions(){
+        Intent intent = new Intent(getContext(), CommonContactsActivity.class);
+        intent.putExtra("did", did);
+        intent.putExtra("type", EXPERIENCE_COMPANION);
+        startActivity(intent);
+    }
+    
+    private void submitOrder(){
+        PlaceOrderDF placeOrderDF;
+        if (mType == Utility.TalentType.EXPERIENCE.ordinal()){
+            placeOrderDF = PlaceOrderDF.newInstance(title, price, did, dataStr, eid, mType);
+        }else {
+            placeOrderDF = PlaceOrderDF.newInstance(title, price, did, dataStr, sid, mType);
+        }
 
-            }
-        });
+        placeOrderDF.show(getFragmentManager(), "PlaceOrderDF");
     }
     
 
@@ -200,23 +195,25 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
         for (int i=0; i<appointDateList.size(); i++){
             //JSONObject dateObject = dateJSONArray.getJSONObject(i);
             LocalDate localDate = appointDateList.get(i).getLocalDate();
-            if (localDate.getDayOfMonth() >= today.getDay()){
+            if (localDate.getDayOfMonth() >= today.getDay() || localDate.getMonthValue() > today.getMonth()){
                 availableDateList.add(localDate);
             }
         }
+
         widget = mDialog.findViewById(R.id.calendarView);
+        widget.setOnDateChangedListener(this);
+        widget.setOnDateLongClickListener(this);
         final LocalDate min = LocalDate.of(today.getYear(), today.getMonth(), today.getDay());
         final LocalDate max = LocalDate.of(today.getYear(), today.getMonth()+3, today.getDay());
-                widget.setOnDateChangedListener(this);
-        widget.setOnDateLongClickListener(this);
         widget.addDecorator(new AvailableDecorator());
+        widget.addDecorator(new DayDisabledDecorator());
         widget.state().edit()
                 .setMinimumDate(min)
                 .setMaximumDate(max)
                 .commit();
     }
 
-    private static class AvailableDecorator implements DayViewDecorator {
+    public static class AvailableDecorator implements DayViewDecorator {
         @Override
         public boolean shouldDecorate(final CalendarDay day) {
             if (availableDateList.size() > 0){
@@ -233,10 +230,28 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
             //view.addSpan(new DotSpan(5, R.color.background));
             view.setBackgroundDrawable(MyApplication.getContext().getResources().getDrawable(R.drawable.hollow_circle));        }
     }
+    
+    private static class DayDisabledDecorator implements DayViewDecorator{
+        @Override
+        public boolean shouldDecorate(final CalendarDay day) {
+            if (availableDateList.size() > 0){
+                if (availableDateList.contains(day.getDate())){
+                    return false;
+                }
+            }
+            return true;
+        }
 
-    private class AppointDate{
+        @Override
+        public void decorate(final DayViewFacade view) {
+            view.setDaysDisabled(true);
+        }
+    }
+
+    public static class AppointDate{
         int did;
-        int tid;
+        int sid;
+        int eid;
         LocalDate localDate;
         int count = 0;
 
@@ -248,22 +263,30 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
             this.did = did;
         }
         
-        public int getTid(){
-            return tid;
+        public int getSid(){
+            return sid;
         }
 
-        public void setTid(int tid){
-            this.tid = tid;
+        public void setSid(int sid){
+            this.sid = sid;
         }
 
-        public LocalDate getLocalDate(){
+        public int getEid(){
+            return eid;
+        }
+
+        public void setEid(int eid){
+            this.eid = eid;
+        }
+        
+         public LocalDate getLocalDate(){
             return localDate;
         }
 
         public void setLocalDate(LocalDate localDate){
             this.localDate = localDate;
         }
-        
+
         public int getCount(){
             return count;
         }
@@ -273,14 +296,26 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
         }
     }
     
+    
+    
             
     private void getAvailableDate(){
-        RequestBody requestBody = new FormBody.Builder()
-                .add("tid", String.valueOf(tid))
-            .add("type", EXPERIENCE_TYPE_GUIDE)
-                .build();
+        availableDateList.clear();
+        appointDateList.clear();
+        FormBody.Builder builder = new FormBody.Builder();
+        String uri = GET_AVAILABLE_APPOINTMENT_DATE;
+        Slog.d(TAG, "-------------------------------->type: "+mType);
+        if (mType == Utility.TalentType.EXPERIENCE.ordinal()){
+            builder.add("eid", String.valueOf(eid))
+                    .add("type", String.valueOf(Utility.TalentType.EXPERIENCE.ordinal()));
+            uri = GET_EXPERIENCE_AVAILABLE_APPOINTMENT_DATE;
+        }else {
+            builder.add("sid", String.valueOf(sid))
+                    .add("type", String.valueOf(Utility.TalentType.GUIDE.ordinal()));
+        }
+        RequestBody requestBody = builder.build();
 
-        HttpUtil.sendOkHttpRequest(getContext(), GET_AVAILABLE_APPOINTMENT_DATE, requestBody, new Callback() {
+        HttpUtil.sendOkHttpRequest(getContext(), uri, requestBody, new Callback() {
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 String responseText = response.body().string();
@@ -288,10 +323,17 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
                     Slog.d(TAG, "==========getAvailableDate response body : " + responseText);
                 if (responseText != null) {
                     try {
-                        JSONObject jsonObject = new JSONObject(responseText);
-                        dateJSONArray = jsonObject.optJSONArray("dates");
-                        processResponse();
-                        myHandler.sendEmptyMessage(GET_AVAILABLE_APPOINTMENT_DATE_DONE);
+                    JSONObject dateObject = dateJSONArray.getJSONObject(i);
+                    appointDate.setDid(dateObject.optInt("did"));
+                    if (mType == Utility.TalentType.GUIDE.ordinal()){
+                        appointDate.setSid(dateObject.optInt("sid"));
+                    }else {
+                        appointDate.setEid(dateObject.optInt("eid"));
+                    }
+                    Slog.d(TAG, "-------------------count: "+dateObject.optInt("count"));
+                    appointDate.setCount(dateObject.optInt("count"));
+                    appointDate.setLocalDate(LocalDate.parse(timeStampToDay(dateObject.optInt("date")), FORMATTER));
+                    appointDateList.add(appointDate);
                     }catch (JSONException e){
                         e.printStackTrace();
                     }
@@ -328,15 +370,29 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
             @NonNull MaterialCalendarView widget,
             @NonNull CalendarDay date,
             boolean selected) {
-
+ dataStr = FORMATTER.format(date.getDate());
         selectWrapper.setVisibility(View.VISIBLE);
 
         if (selected){
             for (int i=0; i< appointDateList.size(); i++){
-                if (appointDateList.get(i).getLocalDate().getDayOfMonth() == date.getDay()){
-                    did = appointDateList.get(i).getDid();
-                    Slog.d(TAG, "---------------onDateSelected------------>did: "+did);
-                }
+                if (appointDateList.get(i).getLocalDate().getMonthValue() == date.getMonth()){
+                    if (appointDateList.get(i).getLocalDate().getDayOfMonth() == date.getDay()){
+                        did = appointDateList.get(i).getDid();
+                        Slog.d(TAG, "---------------onDateSelected------------>did: "+did);
+                        count = appointDateList.get(i).getCount();
+                        if (count > 0){
+                            companionsTV.setText("同行人数"+count);
+                            companionsTV.setOnClickListener(new View.OnClickListener() {
+                                @Override
+                                public void onClick(View view) {
+                                    getCompanions();
+                                }
+                            });
+
+                        }else {
+                            companionsTV.setText("");
+                        }
+                 }
             }
         }
     }
@@ -352,9 +408,6 @@ public class CheckAppointDate extends BaseDialogFragment implements OnDateSelect
         }
     }
 
-    private void submitRoute() {
-        showProgressDialog(getContext().getString(R.string.saving_progress));
-    }
     
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
