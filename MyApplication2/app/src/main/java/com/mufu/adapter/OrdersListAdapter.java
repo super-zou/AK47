a7170f1e9ba1dad92033af0d7550e33c61763d88
super-zou <531387914@qq.com>
2020-10-29 17:47:39 +0800

    Update OrdersListAdapter.java

--- MyApplication2/app/src/main/java/com/mufu/adapter/OrdersListAdapter.java
+++ MyApplication2/app/src/main/java/com/mufu/adapter/OrdersListAdapter.java
@@ -3,11 +3,15 @@ package com.mufu.adapter;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.os.Bundle;
+import android.os.Message;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -15,6 +19,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.mufu.R;
+import com.mufu.common.HandlerTemp;
 import com.mufu.experience.ExperienceDetailActivity;
 import com.mufu.experience.GuideDetailActivity;
 import com.mufu.main.MeetArchiveActivity;
@@ -26,16 +31,25 @@ import com.mufu.util.RoundImageView;
 import com.mufu.util.Slog;
 import com.mufu.util.Utility;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
 import java.util.List;
 
 import static com.mufu.common.MyApplication.getContext;
 import static com.mufu.util.DateUtil.timeStampToMinute;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 /**
  * Created by super-zou on 18-9-21.
  */
 
-public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.ViewHolder> {
+public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.OrderViewHolder> {
 
     private static final String TAG = "OrdersListAdapter";
     private static Context mContext;
@@ -43,9 +57,12 @@ public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.Vi
     private List<OrdersListDF.OrderManager> mOrderList;
     private boolean isScrolling = false;
     private MyItemClickListener mItemClickListener;
+    private RefundConfirmBtnClickListener mRefundConfirmBtnClickListener;
+    private boolean isRefund = false;
 
-    public OrdersListAdapter(Context context) {
+    public OrdersListAdapter(Context context, boolean isRefund) {
         mContext = context;
+        this.isRefund = isRefund;
     }
 
     public void setData(List<OrdersListDF.OrderManager> orderList) {
@@ -53,33 +70,22 @@ public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.Vi
     }
     
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public OrderViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        OrderViewHolder holder = null;
         View view = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.order_queued_item, parent, false);
-        ViewHolder holder = new ViewHolder(view, mItemClickListener);
+        holder = new OrderViewHolder(view, mItemClickListener);
         return holder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull OrdersListAdapter.ViewHolder holder, final int position) {
-        final OrdersListDF.OrderManager order = mOrderList.get(position);
-        setContentView(holder, order);
-
-        holder.itemLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                /*
-                if (mItemClickListener != null){
-                    mItemClickListener.onItemClick(view, position);
-                }
-
-                 */
-                startActivity(order);
-            }
-        });
+    public void onBindViewHolder(final OrderViewHolder holder, final int position) {
+        final OrdersListDF.OrderManager order;
+        order = mOrderList.get(position);
+        setContentView(holder, order, position);
     }
     
-    public void setContentView(OrdersListAdapter.ViewHolder holder, final OrdersListDF.OrderManager order){
+    public void setContentView(OrdersListAdapter.ViewHolder holder, final OrdersListDF.OrderManager order, int position){
 
         holder.titleTV.setText(order.title);
         if (!TextUtils.isEmpty(order.packageName)){
@@ -105,6 +111,24 @@ public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.Vi
         holder.phoneTV.setText(order.phone);
         holder.universityTV.setText(order.university);
         holder.majorTV.setText(order.major);
+        
+        if (isRefund){
+            holder.refundWrapper.setVisibility(View.VISIBLE);
+            holder.payNameTV.setText("账户名："+order.payName);
+            holder.payAccountTV.setText(order.payAccount);
+            holder.refundAmount.setText("金额："+String.valueOf(order.refundAmount));
+            holder.refundReasonTV.setText(order.reason);
+            holder.refundCreated.setText("申请时间："+timeStampToMinute(order.refundCreated));
+            holder.confirmBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    //updateRefundStatus(order.oid, position);
+                    mRefundConfirmBtnClickListener.onRefundConfirmBtnClick(view, position);
+                }
+            });
+        }else {
+            holder.refundWrapper.setVisibility(View.GONE);
+        }
 
         if (order.avatar != null && !"".equals(order.avatar)) {
             Glide.with(getContext()).load(HttpUtil.DOMAIN + order.avatar).into(holder.avatarImg);
@@ -139,6 +163,19 @@ public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.Vi
                 holder.avatarImg.callOnClick();
             }
        });
+        
+       holder.itemLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                /*
+                if (mItemClickListener != null){
+                    mItemClickListener.onItemClick(view, position);
+                }
+
+                 */
+                startActivity(order);
+            }
+        });
     }
     
     private void startActivity(MyOrdersFragmentDF.Order order){
@@ -163,7 +200,7 @@ public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.Vi
         this.isScrolling = isScrolling;
     }
 
-   public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+   public static class OrderViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         MyItemClickListener mListener;
         TextView titleTV;
         TextView packageNameTV;
@@ -181,10 +218,18 @@ public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.Vi
         TextView majorTV;
         ConstraintLayout customerWrapper;
         ConstraintLayout itemLayout;
+       //for refund
+       ConstraintLayout refundWrapper;
+        TextView payNameTV;
+       TextView payAccountTV;
+       TextView refundAmount;
+       TextView refundCreated;
+       TextView refundReasonTV;
+       Button   confirmBtn;
         
-        public ViewHolder(View view, MyItemClickListener myItemClickListener) {
+        public OrderViewHolder(View view, MyItemClickListener myItemClickListener) {
             super(view);
-            itemLayout = view.findViewById(R.id.order_queued_item);
+            itemLayout = view.findViewById(R.id.order_item);
 
             titleTV = view.findViewById(R.id.title);
             packageNameTV = view.findViewById(R.id.package_title);
@@ -201,11 +246,18 @@ public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.Vi
             blockBookingTagTV = view.findViewById(R.id.block_booking_tag);
             blockBookingInfoCL = view.findViewById(R.id.block_booking_info);
             customerWrapper = view.findViewById(R.id.customer_wrapper);
+            payNameTV = view.findViewById(R.id.pay_name);
+            payAccountTV = view.findViewById(R.id.pay_account);
+            refundAmount = view.findViewById(R.id.refund_amount);
+            refundCreated = view.findViewById(R.id.refund_created);
+            refundReasonTV = view.findViewById(R.id.refund_reason);
+            confirmBtn = view.findViewById(R.id.refund_confirm);
+            refundWrapper = view.findViewById(R.id.refund_wrapper);
 
             //将全局的监听赋值给接口
             this.mListener = myItemClickListener;
             Typeface font = Typeface.createFromAsset(getContext().getAssets(), "fonts/fontawesome-webfont_4.7.ttf");
-            FontManager.markAsIconContainer(view.findViewById(R.id.order_queued_item), font);
+            FontManager.markAsIconContainer(view.findViewById(R.id.order_item), font);
         }
         
         /**
@@ -227,13 +279,18 @@ public class OrdersListAdapter extends RecyclerView.Adapter<OrdersListAdapter.Vi
         void onItemClick(View view, int position);
     }
     
+    public interface RefundConfirmBtnClickListener{
+        void onRefundConfirmBtnClick(View view, int position);
+    }
+    
     /**
      * 在activity里面adapter就是调用的这个方法,将点击事件监听传递过来,并赋值给全局的监听
      *
      * @param myItemClickListener
      */
-    public void setItemClickListener(OrdersListAdapter.MyItemClickListener myItemClickListener) {
+    public void setItemClickListener(OrdersListAdapter.MyItemClickListener myItemClickListener, RefundConfirmBtnClickListener refundConfirmBtnClickListener) {
         this.mItemClickListener = myItemClickListener;
+        this.mRefundConfirmBtnClickListener = refundConfirmBtnClickListener;
     }
     
 }
