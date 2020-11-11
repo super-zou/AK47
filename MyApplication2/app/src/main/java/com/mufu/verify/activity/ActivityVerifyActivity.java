40b9fdf47865b542fb3cd1c448abb4f94caf1fd1
super-zou <531387914@qq.com>
2020-11-11 19:12:13 +0800

    Update ActivityVerifyActivity.java

--- MyApplication2/app/src/main/java/com/mufu/verify/activity/ActivityVerifyActivity.java
+++ MyApplication2/app/src/main/java/com/mufu/verify/activity/ActivityVerifyActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 import com.google.android.material.tabs.TabLayout;
 import com.mufu.R;
 import com.mufu.adapter.verify.VerifyFragmentAdapter;
+import com.mufu.util.HttpUtil;
 import com.mufu.verify.talent.TalentPassedFragment;
 import com.mufu.verify.talent.TalentRejectedFragment;
 import com.mufu.verify.talent.TalentRequestFragment;
@@ -18,12 +19,24 @@ import com.mufu.main.MessageFragment;
 import com.mufu.util.BaseFragment;
 import com.mufu.util.FontManager;
 
+import java.io.IOException;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+import static com.mufu.verify.user.UserVerifyActivity.GET_ALL_STATUS_COUNT_DONE;
 
 public class ActivityVerifyActivity extends BaseAppCompatActivity {
 
@@ -39,8 +52,21 @@ public class ActivityVerifyActivity extends BaseAppCompatActivity {
     private List<Fragment> mFragmentList = new ArrayList<>();
 
     private static MyHandler handler;
+        TextView requestCountView;
+    TextView passedCountView;
+    TextView rejectedCountView;
+
+    private int requestCount = 0;
+    private int passedCount = 0;
+    private int rejectedCount = 0;
+
+    public static final int PASS_ACTION = 0;
+    public static final int REJECT_ACTION = 1;
 
     private String[] mTitles = MyApplication.getContext().getResources().getStringArray(R.array.verify_tabs);
+        public static final String GET_EXPERIENCE_VERIFY_AMOUNT_URL = HttpUtil.DOMAIN + "?q=experience/get_experience_verify_amount";
+    public static final String SET_EXPERIENCE_PASSED = HttpUtil.DOMAIN + "?q=verify/set_experience_passed";
+    public static final String SET_EXPERIENCE_REJECTED = HttpUtil.DOMAIN + "?q=verify/set_experience_rejected";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,17 +91,17 @@ public class ActivityVerifyActivity extends BaseAppCompatActivity {
         mTabLayout.addTab(verified_tab);
         mTabLayout.addTab(rejected_tab);
         
-        Fragment unVerifiedFragment = new TalentRequestFragment();
+        Fragment unVerifiedFragment = new ActivityRequestFragment();
         mFragmentList.add(unVerifiedFragment);
-        BaseFragment passedFragment = new TalentPassedFragment();
+        BaseFragment passedFragment = new ActivityRequestFragment();
         mFragmentList.add(passedFragment);
-        Fragment rejectedFragment = new TalentRejectedFragment();
+        Fragment rejectedFragment = new ActivityRequestFragment();
         mFragmentList.add(rejectedFragment);
 
         //创建一个viewpager的adapter
         mFragmentAdapter = new VerifyFragmentAdapter(getSupportFragmentManager(), mFragmentList, mTitles);
         mViewPager.setAdapter(mFragmentAdapter);
-        //mViewPager.setOffscreenPageLimit(0);
+        mViewPager.setOffscreenPageLimit(3);
 
         //将TabLayout和ViewPager关联起来
         mTabLayout.setupWithViewPager(mViewPager);
@@ -119,15 +145,55 @@ public class ActivityVerifyActivity extends BaseAppCompatActivity {
 
             }
         });
+        
+                requestCountView = mTabLayout.getTabAt(0).getCustomView().findViewById(R.id.count);
+        passedCountView = mTabLayout.getTabAt(1).getCustomView().findViewById(R.id.count);
+        rejectedCountView = mTabLayout.getTabAt(2).getCustomView().findViewById(R.id.count);
 
         //unReadView = mTabLayout.getTabAt(2).getCustomView().findViewById(R.id.unread);
         FontManager.markAsIconContainer(findViewById(R.id.tabs), font);
+        
+        getAllCount();
+    }
+    
+     private void getAllCount(){
+        RequestBody requestBody = new FormBody.Builder().build();
+
+        String url = GET_EXPERIENCE_VERIFY_AMOUNT_URL;
+
+        HttpUtil.sendOkHttpRequest(MyApplication.getContext(), url, requestBody, new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (response.body() != null) {
+                    String responseText = response.body().string();
+                    if (responseText != null) {
+                        try {
+                            JSONObject responseObject = new JSONObject(responseText);
+                            requestCount = responseObject.optInt("requesting_amount");
+                            passedCount = responseObject.optInt("passed_amount");
+                            rejectedCount = responseObject.optInt("rejected_amount");
+
+                            handler.sendEmptyMessage(GET_ALL_STATUS_COUNT_DONE);
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, IOException e) {
+            }
+        });
     }
     
     public void handleMessage(Message message) {
-
         switch (message.what) {
-            case MessageFragment.HAVE_UNREAD_MESSAGE:
+            case GET_ALL_STATUS_COUNT_DONE:
+                requestCountView.setText(String.valueOf(requestCount));
+                passedCountView.setText(String.valueOf(passedCount));
+                rejectedCountView.setText(String.valueOf(rejectedCount));
                 break;
             default:
                 break;
