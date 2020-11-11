e33141013a3730eb0a3deed355d61fd0453789a1
super-zou <531387914@qq.com>
2020-11-11 18:16:08 +0800

    Update ParseUtils.java

--- MyApplication2/app/src/main/java/com/mufu/util/ParseUtils.java
+++ MyApplication2/app/src/main/java/com/mufu/util/ParseUtils.java
@@ -124,6 +124,7 @@ public class ParseUtils {
     public static final int ACTIVITY_VERIFIED_NF = 70;
     public static final int ACTIVITY_REJECTED_NF = 71;
     public static final int ORDER_PLACED_NF = 72;
+    public static final int EXPERIENCE_REQUEST_RESULT_NF = 73;
 
     public static final int MALE = 0;
     public static final int FEMALE = 1;
