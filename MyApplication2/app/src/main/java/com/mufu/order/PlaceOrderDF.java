04d0a2b21e5bf65e5109ff0f5cd985b904c6063d
super-zou <531387914@qq.com>
2021-02-26 15:11:30 +0800

    Update PlaceOrderDF.java

--- MyApplication2/app/src/main/java/com/mufu/order/PlaceOrderDF.java
+++ MyApplication2/app/src/main/java/com/mufu/order/PlaceOrderDF.java
@@ -12,11 +12,19 @@ import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
@@ -61,11 +69,15 @@ public class PlaceOrderDF extends BaseDialogFragment {
     private String date;
     private String mPackageName = "";
     private int mAmountPeople = 1;
+        private int mIdentityRequirement = 0;
     private Button submitOrderBtn;
     private TextView totalPriceTV;
     private TextView totalAmountTV;
     private MyHandler myHandler;
     public static final int SUBMIT_ORDER_DONE = 1;
+        private LinearLayout mIdentityRequirementEditWrapper;
+    private ScrollView mPlaceOrderScrollView;
+    private String[] mIdentityArray;
     public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("EEE, d MMM yyyy");
     public static final String PLACE_ORDER = HttpUtil.DOMAIN + "?q=order_manager/place_order";
         private OrderStatusBroadcastReceiver mReceiver;
@@ -74,7 +86,7 @@ public class PlaceOrderDF extends BaseDialogFragment {
     public static final String ORDER_SUBMIT_BROADCAST = "com.mufu.action.ORDER_SUBMIT_BROADCAST";
         public static final String ORDER_EVALUATE_SUCCESS_BROADCAST = "com.mufu.action.ORDER_EVALUATE_SUCCESS_BROADCAST";
     
-    public static PlaceOrderDF newInstance(String title, String packageName, int price, int did, String date, int id, int sold, int maximum, int type) {
+    public static PlaceOrderDF newInstance(String title, String packageName, int price, int did, String date, int id, int sold, int maximum, int type, int identityRequirement) {
         mType = Utility.TalentType.GUIDE.ordinal();
         PlaceOrderDF checkAppointDate = new PlaceOrderDF();
         Bundle bundle = new Bundle();
@@ -89,6 +101,7 @@ public class PlaceOrderDF extends BaseDialogFragment {
         bundle.putInt("id", id);
         bundle.putInt("sold", sold);
         bundle.putInt("maximum", maximum);
+                bundle.putInt("identityRequirement", identityRequirement);
         checkAppointDate.setArguments(bundle);
 
         return checkAppointDate;
@@ -114,6 +127,7 @@ public class PlaceOrderDF extends BaseDialogFragment {
             if (!TextUtils.isEmpty(bundle.getString("packageName", ""))){
                 mPackageName = bundle.getString("packageName");
             }
+                        mIdentityRequirement = bundle.getInt("identityRequirement", 0);
         }
         
         order = new MyOrdersFragmentDF.Order();
@@ -145,6 +159,13 @@ public class PlaceOrderDF extends BaseDialogFragment {
         TextView priceTV = mDialog.findViewById(R.id.price);
         TextView remainingTV = mDialog.findViewById(R.id.remaining_amount);
         TextView maximumTV = mDialog.findViewById(R.id.maximum_amount);
+                mIdentityRequirementEditWrapper = mDialog.findViewById(R.id.identity_information_edit_wrapper);
+        ConstraintLayout identityRequirementWrapper = mDialog.findViewById(R.id.identity_information_wrapper);
+        mPlaceOrderScrollView = mDialog.findViewById(R.id.place_order_scrollview);
+
+        if (mIdentityRequirement > 0){
+            identityRequirementWrapper.setVisibility(View.VISIBLE);
+        }
         if (!TextUtils.isEmpty(mPackageName)){
             packageNameTV.setVisibility(View.VISIBLE);
             packageNameTV.setText(mPackageName);
@@ -156,12 +177,12 @@ public class PlaceOrderDF extends BaseDialogFragment {
         maximumTV.setText(String.valueOf(mMaximumAmount));
         remainingTV.setText("余位"+ (mMaximumAmount - mSoldAmount));
 
+                submitOrderBtn = mDialog.findViewById(R.id.submit_order);
         if (price == 0){
             ConstraintLayout participantsSetting = mDialog.findViewById(R.id.participants_setting);
-            participantsSetting.setVisibility(View.INVISIBLE);
+            participantsSetting.setVisibility(View.GONE);
             submitOrderBtn.setText("参加");
         }
-        submitOrderBtn = mDialog.findViewById(R.id.submit_order);
         submitOrderBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -178,8 +199,83 @@ public class PlaceOrderDF extends BaseDialogFragment {
         processPeopleAmountChange();
         
         registerBroadcast();
+        
+                editIdentityInformation();
 
         return mDialog;
+    }
+    
+        private void editIdentityInformation(){
+        Button addIdentityInfoBtn = mDialog.findViewById(R.id.add_identity_edit);
+        Handler handler = new Handler();
+        addIdentityInfoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                View itemView = LayoutInflater.from(MyApplication.getContext())
+                        .inflate(R.layout.identity_information_edit_item, (ViewGroup) mDialog.findViewById(android.R.id.content), false);
+                mIdentityRequirementEditWrapper.addView(itemView);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        mPlaceOrderScrollView.fullScroll(ScrollView.FOCUS_DOWN);
+                    }
+                });
+            }
+        });
+    }
+    
+    private boolean checkIdentityEditValidation(){
+        int childrenCount = mIdentityRequirementEditWrapper.getChildCount();
+        if (childrenCount == 0){
+            Toast.makeText(getContext(), "请添加参与者身份信息！", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (childrenCount < mAmountPeople){
+            Toast.makeText(getContext(), "参与者身份信息不能少于"+mAmountPeople+"位！", Toast.LENGTH_LONG).show();
+            return false;
+        }else {
+            mIdentityArray = new String[childrenCount];
+            for (int i=0; i<childrenCount; i++){
+                LinearLayout identityItem = (LinearLayout) mIdentityRequirementEditWrapper.getChildAt(i);
+                RadioGroup sexRadioGroup = (RadioGroup) identityItem.getChildAt(0);
+                RadioButton maleRB = (RadioButton)sexRadioGroup.getChildAt(0);
+                int sex = Utility.MALE;
+                if (!maleRB.isChecked()){
+                    sex = Utility.MALE+1;
+                }
+                
+                EditText nameET = (EditText)identityItem.getChildAt(1);
+                int k = i+1;
+                if (TextUtils.isEmpty(nameET.getText().toString())){
+                    Toast.makeText(getContext(), "第"+k+"个用户名不能为空！", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                EditText universityET = (EditText)identityItem.getChildAt(2);
+                if (TextUtils.isEmpty(universityET.getText().toString())){
+                    Toast.makeText(getContext(), "第"+k+"个用户学校不能为空！", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+                EditText identityET = (EditText)identityItem.getChildAt(3);
+                if (TextUtils.isEmpty(identityET.getText().toString())){
+                    Toast.makeText(getContext(), "第"+k+"个用户身份证号不能为空！", Toast.LENGTH_LONG).show();
+                    return false;
+                }
+
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("sex", String.valueOf(sex));
+                    jsonObject.put("realname", nameET.getText().toString());
+                    jsonObject.put("university", universityET.getText().toString());
+                    jsonObject.put("identity_number", identityET.getText().toString());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                
+                String identityString = jsonObject.toString();
+                mIdentityArray[i] = identityString;
+            }
+        }
+        return true;
     }
     
     private void processPeopleAmountChange(){
@@ -242,7 +338,13 @@ public class PlaceOrderDF extends BaseDialogFragment {
 
     private void submitOrder(){
         showProgressDialog(getContext().getString(R.string.submitting_progress));
-        RequestBody requestBody = new FormBody.Builder()
+        if (mIdentityRequirement > 0){
+            if (!checkIdentityEditValidation()){
+                dismissProgressDialog();
+                return;
+            }
+        }
+        FormBody.Builder builder = new FormBody.Builder()
                 .add("did", String.valueOf(did))
                 .add("title", title)
                 .add("number", getOrderNumber(Utility.TalentType.EXPERIENCE.ordinal()))
@@ -251,10 +353,15 @@ public class PlaceOrderDF extends BaseDialogFragment {
                 .add("total_price", totalPriceTV.getText().toString())
                 .add("type", String.valueOf(mType))
                 .add("id", String.valueOf(id))
-                .add("package_name", String.valueOf(mPackageName))
-                .build();
+                .add("package_name", String.valueOf(mPackageName));
+        
+        if (mIdentityRequirement > 0 && mIdentityArray.length> 0){
+            for (int i=0; i<mIdentityArray.length; i++){
+                builder.add("identity_array[]", mIdentityArray[i]);
+            }
+        }
 
-        HttpUtil.sendOkHttpRequest(getContext(), PLACE_ORDER, requestBody, new Callback() {
+        HttpUtil.sendOkHttpRequest(getContext(), PLACE_ORDER, builder.build(), new Callback() {
         @Override
             public void onResponse(Call call, Response response) throws IOException {
                 String responseText = response.body().string();
